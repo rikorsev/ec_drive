d50cc7767c85d33bf34a14f25d8965e1b9695f6b
Alexandr Kolosov <rikorsev@mail.ru>
2018-08-05 19:48:44 +0300

    Add static declaration for ringbuffer

--- fmw/lib/abst/ringbuffer/ringbuffer.h
+++ fmw/lib/abst/ringbuffer/ringbuffer.h
@@ -1,14 +1,26 @@
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
+#include <stdint.h>
+#include <stdlib.h>
+
 typedef struct
 {
   void* buff;
   size_t size;
   size_t idx_in;
   size_t idx_out;
-
 }ring_buffer_t;
+
+#define DECLARE_RING_BUFFER(name, sz)                  \
+  static unsigned char rb_##name##_buff[(sz)] = {0};   \
+  ring_buffer_t name =				       \
+  {                                                    \
+    .buff      = rb_##name##_buff,                     \
+    .size      = (sz),  	                       \
+    .idx_in    = 0,				       \
+    .idx_out   = 0,				       \
+  };                                            
 
 ring_buffer_t* ring_buffer_create(size_t sz);
 void ring_buffer_delete(ring_buffer_t* prb);
