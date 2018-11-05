af9ba702d756a428c2182e2377600a2c8f217781
Alexandr Kolosov <rikorsev@mail.ru>
2018-11-05 20:35:21 +0200

    Remove commented initialisation of HSI clock

--- fmw/bsp/stm32/stm32f05x/ec_drive_v2_board/clock/ecd_clock.c
+++ fmw/bsp/stm32/stm32f05x/ec_drive_v2_board/clock/ecd_clock.c
@@ -3,21 +3,12 @@
 
 void ecd_clock_init(void)
 {
-  /* RCC_HSICmd(ENABLE); */
-
-  /* while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) != SET)     */
-  /*   { */
-  /*     /\* do nothing *\/ */
-  /*   } */
-    
-  /* RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI); */
-
   RCC_HSEConfig(RCC_HSE_ON);
 
   if(RCC_WaitForHSEStartUp() != SUCCESS)
-    {
-      assert(0);
-    }
+  {
+    assert(0);
+  }
 
   RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE);
 }
